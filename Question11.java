ab573c1eb571b5e8ce80e4794da320f3b6ef4965
SanketDhakate <126185900+SanketDhakate@users.noreply.github.com>
2024-12-30 15:32:41 +0530

    Question11.java

--- Question11.java
+++ Question11.java
@@ -1,8 +1,7 @@
 /* Print all even numbers from 1 to 100. */
 public class Question11 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            if (i % 2 == 0) {
+        for (int i = 2; i <= 100; i+=2) {
                 System.out.println(i);
             }
         }  
